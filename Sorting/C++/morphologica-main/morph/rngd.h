aadab27aaa406979fcb8dd992eb5abe9f12648e9
DavidTesar <73195563+DavidTesar@users.noreply.github.com>
2021-12-10 14:45:41 -0600

    Remove Library

--- Sorting/C++/morphologica-main/morph/rngd.h
+++
@@ -1,9 +0,0 @@
-// Include only morph::randDouble.h from rng.h
-#pragma once
-#ifdef RANDSINGLE
-# undef RANDSINGLE
-#endif
-#ifdef NO_RANDDOUBLE
-# undef NO_RANDDOUBLE
-#endif
-#include <morph/rng.h>
--- Sorting/C++/morphologica-main/morph/rngs.h
+++
@@ -1,5 +0,0 @@
-// Include only morph::randSingle.h from rng.h
-#pragma once
-#define RANDSINGLE 1
-#define NO_RANDDOUBLE 1
-#include <morph/rng.h>
